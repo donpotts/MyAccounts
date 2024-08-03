a0f6a4106fd3d6c96924329a6b1fc067deb06070
Don Potts <don.potts@beverycreative.com>
2024-08-03 17:10:25 -0500

    WIP save point

---
+++ MyAccounts.Maui.Blazor/wwwroot/index.js
@@ -0,0 +1,17 @@
+﻿window.downloadFromBase64 = function (base64, filename) {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    var byteCharacters = atob(base64);
+    var byteNumbers = new Array(byteCharacters.length);
+    for (var i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    var byteArray = new Uint8Array(byteNumbers);
+    var blob = new Blob([byteArray], { type: "application/octet-stream" });
+    var url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+};
