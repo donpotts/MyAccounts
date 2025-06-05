487c0476560f1b0551d41ad2e55007c5bc04f799
Don Potts <don.potts@beverycreative.com>
2025-06-05 16:02:49 -0500

    Added support for Excel Budget file to BlazorDatasheet

--- MyAccounts/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MyAccounts/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace MyAccounts.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.11")
+                .HasAnnotation("ProductVersion", "8.0.16")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
